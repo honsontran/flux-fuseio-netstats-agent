3232ebef798efc6ec531a779db273e58f48e0e01
Andrew Pohl <andypohl@hotmail.co.uk>
2020-12-04 13:04:53 +0000

    Add role and extended version info

--- run.sh
+++ run.sh
@@ -25,6 +25,22 @@ for (( i=0; i<${#ARG_VEC[@]}; i++ )) ; do
     bridge_version="${ARG_VEC[$nextIndex]}"
     i=$nextIndex
 
+  elif [[ $arg == --role ]] ; then
+    role="${ARG_VEC[$nextIndex]}"
+    i=$nextIndex
+
+  elif [[ $arg == --parity-version ]] ; then
+    parity_version="${ARG_VEC[$nextIndex]}"
+    i=$nextIndex
+
+  elif [[ $arg == --fuseapp-version ]] ; then
+    fuseapp_version="${ARG_VEC[$nextIndex]}"
+    i=$nextIndex
+
+  elif [[ $arg == --netstats-version ]] ; then
+    netstats_version="${ARG_VEC[$nextIndex]}"
+    i=$nextIndex
+
   # A not known argument.
   else
     echo Unkown argument: $arg
@@ -33,6 +49,5 @@ for (( i=0; i<${#ARG_VEC[@]}; i++ )) ; do
 done
 
 cd /home/ethnetintel/eth-net-intelligence-api
-jq -r --arg in "${instance_name}" --arg bv "${bridge_version}" '.[0].env.INSTANCE_NAME |= $in | .[0].env.BRIDGE_VERSION |= $bv | .[0].env.WS_SECRET |= "i5WsUJWaMUHOS2CwvTRy"' app.json.example > app.json
-/usr/bin/pm2 start ./app.json
+jq -r --arg in "${instance_name}" --arg bv "${bridge_version}" --arg r "${role}" --arg pv "${parity_version}" --arg fv "${fuseapp_version}" --arg nv "${netstats_version}" '.[0].env.INSTANCE_NAME |= $in | .[0].env.BRIDGE_VERSION |= $bv | .[0].env.ROLE |= $r | .[0].env.FUSE_APP_VERSION |= $fv | .[0].env.PARITY_VERSION |= $pv | .[0].env.NETSTATS_VERSION |= $nv |.[0].env.WS_SECRET |= "i5WsUJWaMUHOS2CwvTRy"' app.json.example > app.json/usr/bin/pm2 start ./app.json
 /usr/bin/pm2 logs netstat_daemon --lines 1000
